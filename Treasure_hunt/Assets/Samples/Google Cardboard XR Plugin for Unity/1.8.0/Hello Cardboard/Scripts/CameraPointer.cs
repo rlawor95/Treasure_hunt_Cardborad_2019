212d7214d989d0603afed76c5c32a51710aa06e5
KimJack <kjk71978292@gmail.com>
2021-10-18 00:28:37 +0900

    1

--- Treasure_hunt/Assets/Samples/Google Cardboard XR Plugin for Unity/1.8.0/Hello Cardboard/Scripts/CameraPointer.cs
+++ Treasure_hunt/Assets/Samples/Google Cardboard XR Plugin for Unity/1.8.0/Hello Cardboard/Scripts/CameraPointer.cs
@@ -17,7 +17,17 @@
 //-----------------------------------------------------------------------
 
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using DG.Tweening;
+#if UNITY_2017_2_OR_NEWER
+using UnityEngine.XR;
+#else
+using XRSettings = UnityEngine.VR.VRSettings;
+#endif
 
 /// <summary>
 /// Sends messages to gazed GameObject.
@@ -26,21 +36,76 @@ public class CameraPointer : MonoBehaviour
 {
     private const float _maxDistance = 10;
     private GameObject _gazedAtObject = null;
+    EventSystem m_eventSystem;
+    PointerEventData m_pointerEvent;
+
+    public Image GazeImage;
+
+    public Selectable m_currentSelectable;
+    IPointerClickHandler m_clickHandler;
+
+
+    void Start()
+    {
+        m_eventSystem = EventSystem.current;
+        m_pointerEvent = new PointerEventData(m_eventSystem);
+        m_pointerEvent.button = PointerEventData.InputButton.Left;
+    }
 
     /// <summary>
     /// Update is called once per frame.
     /// </summary>
     public void Update()
     {
-        // Casts ray towards camera's forward direction, to detect if a GameObject is being gazed
-        // at.
-        RaycastHit hit;
-        int mask =1 << 8 ;
-        if (Physics.Raycast(transform.position, transform.forward, out hit, _maxDistance,mask))
+        if(GameManager.GameStart)
         {
-        
-            Debug.Log("hit :  "+hit.transform.name);
-            // GameObject detected in front of the camera.
+            GameRaycast();
+        }
+        else
+        {
+            UIRaycast();
+        }
+
+    }
+
+    private void UIRaycast()
+    {
+        m_pointerEvent.position =
+#if UNITY_EDITOR
+            new Vector2(Screen.width / 2, Screen.height / 2);
+#else
+            new Vector2(XRSettings.eyeTextureWidth / 2, XRSettings.eyeTextureHeight / 2);
+#endif
+
+        List<RaycastResult> raycastResults = new List<RaycastResult>();
+        m_eventSystem.RaycastAll(m_pointerEvent, raycastResults);
+
+        if (raycastResults.Count > 0)
+        {
+            var newSelectable = raycastResults[0].gameObject.GetComponentInParent<Selectable>();
+          
+            if(m_currentSelectable != newSelectable)
+            {
+                GazeOff();
+                Debug.Log("gaze  " + newSelectable);
+                GazeOn(newSelectable);
+                m_currentSelectable = newSelectable;
+            }
+        }
+        else
+        {
+            m_currentSelectable = null;
+            GazeOff();
+        }
+    }
+
+    private void GameRaycast()
+    {
+        RaycastHit hit;
+        int mask = (1 << 8);
+
+        if (Physics.Raycast(transform.position, transform.forward, out hit, _maxDistance, mask))
+        {
             if (_gazedAtObject != hit.transform.gameObject)
             {
                 // New GameObject.
@@ -61,5 +126,20 @@ public class CameraPointer : MonoBehaviour
         {
             _gazedAtObject?.SendMessage("OnPointerClick");
         }
+    }
+
+    void GazeOn(Selectable selectable)
+    {
+        m_clickHandler = selectable.GetComponent<IPointerClickHandler>();
+
+        GazeImage.DOFillAmount(1,1.0f).OnComplete(() =>{
+            Debug.Log("Gaze " + selectable.name);
+            m_clickHandler.OnPointerClick(m_pointerEvent);
+        });
+    }
+
+    void GazeOff()
+    {
+        GazeImage.fillAmount = 0;
     }
 }
---
+++ Treasure_hunt/Assets/Script/GameManager.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public static bool GameStart = false;
+    public Text TimeText;
+    public Text TreasureCntText;
+    public Toggle InfToggle;
+    
+
+    public int Time = 3;
+    public int TreasureCnt = 1;
+
+    public Canvas canvas;
+    
+
+    void Start()
+    {
+        TimeText.text = Time.ToString();
+         TreasureCntText.text = TreasureCnt.ToString();
+    }
+
+    public void StartClick()
+    {
+        canvas.gameObject.SetActive(false);
+        GameStart = true;
+    }
+
+    public void TreasureIncrease()
+    {
+        Debug.Log("TreasureIncrease ");
+        if (TreasureCnt < 10)
+        {
+            TreasureCnt++;
+            TreasureCntText.text = TreasureCnt.ToString();
+        }
+
+    }
+    public void TreasureDecrease()
+    {
+        Debug.Log("TreasureDecrease ");
+        if (TreasureCnt > 1)
+        {
+            TreasureCnt--;
+            TreasureCntText.text = TreasureCnt.ToString();
+        }
+    }
+
+    public void TimeIncrease()
+    {
+        Debug.Log("TimeIncrease ");
+        if (Time > 10)
+        {
+            TimeText.text = "INF";
+        }
+        else
+        {
+            Time++;
+            TimeText.text = Time.ToString();
+        }
+    }
+
+    public void TimeDecrease()
+    {
+        if (Time > 3)
+        {
+            Time--;
+            TimeText.text = Time.ToString();
+        }
+    }
+
+    public void TimeInf()
+    {
+        if (InfToggle.isOn)
+            TimeText.text = "INF";
+        else
+            TimeText.text = Time.ToString();
+    }
+}
